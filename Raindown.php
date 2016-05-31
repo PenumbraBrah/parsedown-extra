bb2752b8fd522f55536506e4e517e643705580db
Jay Andorian <jayandorian@gmail.com>
2016-05-31 17:09:05 -0400

    ADD: Raindown.php

---
+++ Raindown.php
@@ -0,0 +1,38 @@
+<?php
+
+#
+#
+# Raindown
+# https://github.com/PenumbraBrah/raindown
+#
+# (c) Jay Andorian
+#
+# For the full license information, view the LICENSE file that was distributed
+# with this source code.
+#
+#
+
+class Raindown extends ParsedownExtra
+{
+    function __construct()
+    {
+    }
+
+    function content($text)
+    {
+        if (strpos($text, '<raindown>') !== false) {
+            list($meta, $content) = explode('<raindown>', $text, 2);
+            return parent::text($content);
+        }
+        return parent::text($text);
+    }
+
+    function meta($text)
+    {
+        if (strpos($text, '<raindown>') !== false) {
+            list($meta, $content) = explode('<raindown>', $text, 2);
+            return $meta;
+        }
+        return false;
+    }
+}
